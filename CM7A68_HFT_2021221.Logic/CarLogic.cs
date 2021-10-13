cef40a18380a77b56dee4dac051e3f52f28d1d09
balintSly <sziszko0129@gmail.com>
2021-10-14 00:07:07 +0200

    CarLogic CRUD done

--- CM7A68_HFT_2021221.Logic/CarLogic.cs
+++ CM7A68_HFT_2021221.Logic/CarLogic.cs
@@ -1,10 +1,73 @@
 ﻿using System;
-using 
+using System.Collections.Generic;
+using CM7A68_HFT_2021221.Models;
+using CM7A68_HFT_2021221.Repository;
 
 namespace CM7A68_HFT_2021221.Logic
 {
-    public class CarLogic
+    public class CarLogic : ICarLogic
     {
+        ICarRepo carRepo;
+        public CarLogic(ICarRepo carRepo)
+        {
+            this.carRepo = carRepo;
+        }
+        public void Create(Car car)
+        {
+            if (car.Cylinder_capacity < 0)
+            {
+                throw new ArgumentException("Invalid cylinder capacity.")
+            }
+            if (car.Cylinder_number < 0)
+            {
+                throw new ArgumentException("Invalid cylinder number.")
+            }
+            if (car.Model.Length == 0)
+            {
+                throw new ArgumentException("Invalid model name.");
+            }
+            carRepo.Create(car);
+        }
+        public Car Read(int id)
+        {
+            try
+            {
+                return carRepo.Read(id);
+            }
+            catch (Exception)
+            {
 
+                throw new ArgumentException("There is no car with the following ID: " + id);
+            }
+        }
+        public void Update(Car car)
+        {
+            try
+            {
+                Read(car.ID);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message);
+            }
+            carRepo.Update(car);
+        }
+        public IEnumerable<Car> ReadAll()
+        {
+            return carRepo.ReadAll();
+        }
+        public void Delete(int id)
+        {
+            try
+            {
+                carRepo.Delete(id);
+            }
+            catch (Exception)
+            {
+
+                throw new ArgumentException("There is no car with the following ID: " + id);
+            }
+
+        }
     }
 }
---
+++ CM7A68_HFT_2021221.Logic/ICarLogic.cs
@@ -0,0 +1,14 @@
+﻿using CM7A68_HFT_2021221.Models;
+using System.Collections.Generic;
+
+namespace CM7A68_HFT_2021221.Logic
+{
+    public interface ICarLogic
+    {
+        void Create(Car car);
+        void Delete(int id);
+        Car Read(int id);
+        IEnumerable<Car> ReadAll();
+        void Update(Car car);
+    }
+}
