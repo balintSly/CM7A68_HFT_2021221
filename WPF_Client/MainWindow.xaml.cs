5920a2b10598321fa129c28f39e3a3b50ca7a8cb
balintsly <balintsly@stud.uni-obuda.hu>
2022-01-28 14:19:34 +0100

    description text added to wpf

---
+++ WPF_Client/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using CM7A68_HFT_2021221.Models;
+
+namespace WPF_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void themeToggle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
